082d8e1734f57b5e52b66b04682457662ed58d17
cocococco <kyeonsoo26@gmail.com>
2020-01-26 20:29:03 +0900

    1874 스택수열 소스, 주석

--- 10828 Stack/1874 스택 수열/1874.cpp
+++ 10828 Stack/1874 스택 수열/1874.cpp
@@ -8,47 +8,47 @@ int main()
 {
 	stack<int> s;
 	int n;
-	string ans;
+	string ans; // 결과값을 string으로 만듦
 
-	cin >> n;
+	cin >> n; // n개 입력받을 예정
 
 	int m = 0;
 
 	while (n--)
 	{
 		int x;
-		cin >> x;
+		cin >> x; // 입력
 		if (x > m)
 		{
-			while (x > m)
+			while (x > m) // x == m 일 때 빠져나옴, x = 4
 			{
-				s.push(++m);
+				s.push(++m); // m = 0,1,2,3, s = 1,2,3,4
 				ans += '+';
 			}
-			s.pop();
+			s.pop(); // x == m 이면 pop
 			ans += '-';
 		}
-		else
+		else // 입력받은 숫자가 이미 스택에 있을 때, x = 3
 		{
 			bool found = false;
 			if (!s.empty())
 			{
-				int top = s.top();
-				s.pop();
+				int top = s.top(); // top = 3
+				s.pop(); // top을 pop한 게 x면 성공
 				ans += '-';
 				if (x == top)
 				{
 					found = true;
 				}
 			}
-			if (!found)
+			if (!found) // x와 다르면 실패
 			{
 				cout << "NO" << '\n';
 				return 0;
 			}
 		}
 	}
-	for (auto x : ans)
+	for (auto x : ans) // auto를 사용하는 이유? 범위 기반 for문(ranged-based for loop)에서 auto의 사용은 이상적인 방식. auto는 자동으로 자료형을 부여받음. 대신 초기화 필요
 	{
 		cout << x << '\n';
 	}

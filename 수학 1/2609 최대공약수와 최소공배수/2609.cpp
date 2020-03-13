c810b1dca00099e4076279f555a2d7d2c7464650
cocococco <kyeonsoo26@gmail.com>
2020-03-13 19:19:51 +0900

    [300 수학 1] 최대공약수 2609 최대공약수와 최소공배수

---
+++ 수학 1/2609 최대공약수와 최소공배수/2609.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a, b;
+//	cin >> a >> b;
+//	int x, y;
+//	x = a;
+//	y = b;
+//
+//	while (y != 0)
+//	{
+//		int r = x % y;
+//		x = y;
+//		y = r;
+//	}
+//	cout << x << '\n' << a * b / x << '\n';
+//}

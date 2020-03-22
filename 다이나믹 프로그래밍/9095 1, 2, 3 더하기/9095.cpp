7df7e5ae134d2e65413480c2c4058997a498b6c8
cocococco <kyeonsoo26@gmail.com>
2020-03-22 19:07:50 +0900

    omit

---
+++ 다이나믹 프로그래밍/9095 1, 2, 3 더하기/9095.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int d[12];
+//	d[1] = 1;
+//	d[2] = 2;
+//	d[3] = 4;
+//
+//	for (int i = 4; i <= 11; i++)
+//	{
+//		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+//	}
+//
+//	int t;
+//	cin >> t;
+//	while (t--)
+//	{
+//		int n;
+//		cin >> n;
+//		cout << d[n] << '\n';
+//	}
+//	return 0;
+//}

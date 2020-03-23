3e2b381e76fa7f191894b2667bd463a211a873b3
cocococco <kyeonsoo26@gmail.com>
2020-03-23 21:57:33 +0900

    [600 그래프 1] 13023 ABCDE

---
+++ 그래프/그래프/13023.cpp
@@ -0,0 +1,68 @@
+/* 원래 이걸로 구현하지 않지만 인접 행렬, 인접 리스트, 간선 리스트를 사용하기 위해 억지로 이런 식으로 구현한 것 */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool a[2000][2000]; // 인접 행렬
+vector<int> g[2000]; // 인접 리스트
+vector<pair<int, int>> edges; // 간선 리스트
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 0; i < m; i++)
+	{
+		int from, to;
+		cin >> from >> to;
+		edges.push_back({ from, to });
+		edges.push_back({ to, from });
+
+		a[from][to] = a[to][from] = true;
+
+		g[from].push_back(to);
+		g[to].push_back(from);
+	}
+
+	m *= 2;
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			// A -> B
+			int A = edges[i].first;
+			int B = edges[i].second;
+			// C -> D
+			int C = edges[j].first;
+			int D = edges[j].second;
+
+			if (A == B || A == C || A == D || B == C || B == D || C == D)
+			{
+				continue;
+			}
+
+			// B -> C
+			if (!a[B][C])
+			{
+				continue;
+			}
+			// D -> E
+			for (int E : g[D])
+			{
+				if (A == E || B == E || C == E || D == E)
+				{
+					continue;
+				}
+
+				cout << 1 << '\n';
+				return 0;
+			}
+		}
+	}
+
+	cout << 0 << '\n';
+	return 0;
+}

d2a7e9c010f23c45067b723e0904202704e92773
cocococco <kyeonsoo26@gmail.com>
2020-04-26 22:56:41 +0900

    [530 브루트 포스 - 재귀] 6603 로또 omit

---
+++ 브루트 포스 - 재귀/1182 부분수열의 합/1182.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, s, cnt = 0;
+vector<int> a;
+
+void go(int index, int sum)
+{
+	if (sum == s && index == n)
+	{
+		cnt++;
+		return;
+	}
+	if (index == n && sum != s) return;
+
+	go(index + 1, sum + a[index]);
+
+	go(index + 1, sum);
+
+	return;
+}
+
+int main()
+{
+	cin >> n >> s;
+
+	for (int i = 0; i < n; i++)
+	{
+		int tmp;
+		cin >> tmp;
+		a.push_back(tmp);
+	}
+	go(0, 0);
+	cout << cnt << '\n';
+
+	return 0;
+}

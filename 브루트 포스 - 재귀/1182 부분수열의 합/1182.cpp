eb8479c6858eb707a8610a2f65b0f6e6cc1e7c15
cocococco <kyeonsoo26@gmail.com>
2020-05-01 21:01:40 +0900

    [531 브루트 포스 - 재귀(연습)] 1182 부분 수열의 합 완료

--- 브루트 포스 - 재귀/1182 부분수열의 합/1182.cpp
+++ 브루트 포스 - 재귀/1182 부분수열의 합/1182.cpp
@@ -1,4 +1,3 @@
-// 다시 봐야함 왜틀렸는지 모르겠음
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -35,6 +34,12 @@ int main()
 		a.push_back(tmp);
 	}
 	go(0, 0);
+
+	if (s == 0) cnt -= 1; // 중요!
+	// 아무것도 선택하지 않는 공집합이 있을 수 있다.
+	// 그러므로 합이 0인 것을 찾을 때에는 공집합 하나를
+	// 빼 줘야 한다.
+
 	cout << cnt << '\n';
 
 	return 0;

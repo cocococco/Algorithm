c08a6c1d763f5bf4ed6b48dd2e8af767daf46517
cocococco <kyeonsoo26@gmail.com>
2020-05-03 16:22:59 +0900

    15657 N과 M 8 주석 추가

--- Brute Force - N and M/15657 N과 M 8/15657_1 baekjoon.cpp
+++ Brute Force - N and M/15657 N과 M 8/15657_1 baekjoon.cpp
@@ -3,39 +3,45 @@ N개의 서로 다른 자연수 중에서 M개를 고른 수열
 (중복 가능, 비내림차순)
 */
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int cnt[10]; // 여러 개 선택할 수 있으므로 만든 cnt 배열
+int num[10];
 
 // 선택하는 경우, 안하는 경우로 나누는 방법
 void go(int index, int selected, int n, int m)
 {
 	if (selected == m) // selected가 인덱스임
 	{
-		for (int i = 1; i <= n; i++)
+		// 0번째 인덱스부터 n-1번째까지
+		for (int i = 0; i < n; i++)
 		{
 			for (int j = 1; j <= cnt[i]; j++)
 			{
-				cout << i << ' ';
+				cout << num[i] << ' ';
+				// 해당 i 인덱스에 들어있는 cnt 개수만큼
+				// num의 i 인덱스에 있는 숫자를 출력함
 			}
 		}
 		cout << '\n';
 		return;
 	}
 
-	if (index > n) return; // 불가능한 경우
+	if (index >= n) return; // 불가능한 경우
 
 	// 같은 걸 여러 개 선택할 수 있으므로 루프를 만들고
 	// cnt 배열을 사용하여 선택하는 경우를 따진다
 	// 1 1 1과 1 1 중 1 1 1이 순서가 더 빠르므로
 	// 거꾸로 루프를 타야 함
-	// 남은 빈칸의 개수 ~ 1까지 중에서 선택
+	// i는 남은 빈칸의 개수, m ~ 1까지
 	for (int i = m - selected; i >= 1; i--)
 	{
 		// 선택한 경우
 		cnt[index] = i; // cnt : i를 몇개 선택했는지 저장
 		go(index + 1, selected + i, n, m);
+		// i만큼 선택했으므로 selected는 i만큼 증가
 	}
 
 	// 선택하지 않은 경우
@@ -47,8 +53,13 @@ int main()
 {
 	int n, m;
 	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num[i];
+	}
+	sort(num, num + n);
 
-	go(1, 0, n, m);
+	go(0, 0, n, m);
 
 	return 0;
 }

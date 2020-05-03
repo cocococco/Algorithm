6ff208507b85dbed37cf87597cac74097f5ed863
cocococco <kyeonsoo26@gmail.com>
2020-05-03 16:17:14 +0900

    15652 N과 M 4 주석 추가

--- Brute Force - N and M/15652 N과 M 4/15652_1.cpp
+++ Brute Force - N and M/15652 N과 M 4/15652_1.cpp
@@ -13,13 +13,16 @@ void go(int index, int selected, int n, int m)
 {
 	if (selected == m) // selected가 인덱스임
 	{
-		for (int i = 1; i <= n; i++)
+		for (int i = 1; i <= n; i++) // 고를 수 있는 숫자는 1~n
 		{
+			// 해당 숫자의 인덱스를 가진 cnt 배열에 들어있는
+			// 개수만큼 해당 숫자를 출력함
+			// cnt[1]에는 1이 몇 개 있는지 들어있음
 			for (int j = 1; j <= cnt[i]; j++)
 			{
 				cout << i << ' ';
 			}
-		}
+		} // 이걸 n까지 돌면서 찾고 출력
 		cout << '\n';
 		return;
 	}
@@ -30,12 +33,13 @@ void go(int index, int selected, int n, int m)
 	// cnt 배열을 사용하여 선택하는 경우를 따진다
 	// 1 1 1과 1 1 중 1 1 1이 순서가 더 빠르므로
 	// 거꾸로 루프를 타야 함
-	// 남은 빈칸의 개수 ~ 1까지 중에서 선택
+	// i는 남은 빈칸의 개수, m ~ 1까지
 	for (int i = m - selected; i >= 1; i--)
 	{
 		// 선택한 경우
 		cnt[index] = i; // cnt : i를 몇개 선택했는지 저장
 		go(index + 1, selected + i, n, m);
+		// i만큼 선택했으므로 selected는 i만큼 증가
 	}
 
 	// 선택하지 않은 경우

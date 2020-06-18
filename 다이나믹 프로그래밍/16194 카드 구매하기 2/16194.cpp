3ec8246940a1f95e70c39e063499f78dfdb3b23d
cocococco <kyeonsoo26@gmail.com>
2020-06-18 14:57:24 +0900

    [400 다이나믹 프로그래밍] 16194 카드 구매하기 2 미완

---
+++ 다이나믹 프로그래밍/16194 카드 구매하기 2/16194.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int d[1001];
+int p[1000];
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> p[i];
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			d[i] = min(d[i], d[i - j] + p[j]);
+		}
+	}
+
+	cout << d[n] << '\n';
+	return 0;
+}

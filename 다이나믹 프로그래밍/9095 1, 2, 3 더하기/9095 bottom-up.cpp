b78188211689ebaed293b538ac75a78afd0f6394
cocococco <39549884+cocococco@users.noreply.github.com>
2024-09-21 16:42:57 +0900

    ver17 -> 22 dp

--- 다이나믹 프로그래밍/9095 1, 2, 3 더하기/9095 bottom-up.cpp
+++ 다이나믹 프로그래밍/9095 1, 2, 3 더하기/9095 bottom-up.cpp
@@ -1,25 +1,25 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	int t;
-//	cin >> t;
-//	while (t--)
-//	{
-//		int n;
-//		cin >> n;
-//		int d[12];
-//		d[1] = 1;
-//		d[2] = 2;
-//		d[3] = 4;
-//
-//		for (int i = 4; i <= n; i++)
-//		{
-//			d[i] = d[i - 1] + d[i - 2] + d[i - 3];
-//		}
-//		cout << d[n] << '\n';
-//	}
-//	return 0;
-//}
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int d[12];
+		d[1] = 1;
+		d[2] = 2;
+		d[3] = 4;
+
+		for (int i = 4; i <= n; i++)
+		{
+			d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+		}
+		cout << d[n] << '\n';
+	}
+	return 0;
+}
